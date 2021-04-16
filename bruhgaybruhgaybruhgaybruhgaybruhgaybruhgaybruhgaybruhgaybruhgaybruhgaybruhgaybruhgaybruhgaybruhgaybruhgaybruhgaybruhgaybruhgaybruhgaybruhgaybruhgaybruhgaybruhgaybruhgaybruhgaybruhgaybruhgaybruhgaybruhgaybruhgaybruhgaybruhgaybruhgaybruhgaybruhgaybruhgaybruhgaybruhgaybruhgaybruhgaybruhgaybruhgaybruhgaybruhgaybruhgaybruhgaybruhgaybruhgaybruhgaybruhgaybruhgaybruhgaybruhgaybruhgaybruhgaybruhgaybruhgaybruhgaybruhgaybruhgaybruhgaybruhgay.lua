bc19feed6934a1298ab4294db09d2a595a5df195
mainpaste-ro <82179958+mainpaste-ro@users.noreply.github.com>
2021-04-16 18:10:42 +0200

    Update and rename bruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgay.lua to thehack.lua

--- bruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgaybruhgay.lua
+++
@@ -1,4 +0,0 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/Ghg5TARz", true))()
--- game: https://web.roblox.com/games/2788229376/
--- youtube: goodtesterhacks
--- subcribe ;)
---
+++ thehack.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/Ghg5TARz", true))()
+--        //               ^ the script ^                   \\        --
+--[[
+  game: https://web.roblox.com/games/2788229376/
+  youtube: goodtesterhacks
+  subcribe ;)
+]]
