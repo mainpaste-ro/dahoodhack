72a84e24e4981c1c3fb303d0d6c96061b80e8979
mainpaste-ro <82179958+mainpaste-ro@users.noreply.github.com>
2021-04-16 18:23:21 +0200

    Update thehack.lua

--- thehack.lua
+++ thehack.lua
@@ -1,8 +1,13 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/Ghg5TARz", true))()
---        //               ^ the script ^                   \\        --
---[[
-  game: https://web.roblox.com/games/2788229376/
-  youtube: goodtesterhacks
-  subcribe ;)
-  download script: https://pastebin.pl/view/download/e7e88aa9
+--[[  info:
+  use a krnl or synapse x or hectasploit reborn to execute script.
+  download hectasploit reborn: https://mega.nz/folder/cU9EwQ7L#AyIsLTea9-q0jnhjsSsRrA
+  download krnl: https://wearedevs.net/d/Krnl
+  download synapse x: https://x.synapse.to/
 ]]
+--[[ info script:
+  game: https://web.roblox.com/games/2788229376/
+  download script: https://pastebin.pl/view/download/e7e88aa9
+  made by: Racemodex#0001
+  discord: https://discord.gg/racecodex
+]]
+loadstring(game:HttpGet("https://pastebin.com/raw/Ghg5TARz", true))()
